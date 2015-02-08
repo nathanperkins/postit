2f3cd4b5c34e0d36bcdb305471d6315768114346
Nathan Perkins <nperkins487@gmail.com>
2015-02-08 00:18:48 -0800

    posts, users, comments and categories created

--- db/migrate/20150208052859_user_post_association.rb
+++
@@ -1,9 +0,0 @@
-class UserPostAssociation < ActiveRecord::Migration
-  def change
-    change_table :posts do |t|
-      t.references :user
-    end
-
-    add_foreign_key :posts, :users
-  end
-end
--- db/migrate/20150208055038_create_comments.rb
+++ db/migrate/20150208055038_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.text :body
       t.integer :user_id, :post_id
+      t.timestamps
     end
   end
 end
