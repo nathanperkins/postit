838e9091173e590ab8871f22f6fc12045eaab924
Nathan Perkins <nperkins487@gmail.com>
2015-03-03 23:35:40 -0800

    added users dropdown to nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+  end
+
   private
 
     def user_params
